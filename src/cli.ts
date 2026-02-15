08a25a4575c0e77e9f261be65caa478f657e866d
WAVE <wave745@github.com>
2026-02-15 22:07:12 +0100

    chore: bump version to 3.4.2

--- src/cli.ts
+++ src/cli.ts
@@ -30,7 +30,7 @@ const program = new Command();
 program
   .name('forge')
   .description('FORGE - Intent-driven app assembly on Solana')
-  .version('3.4.1');
+  .version('3.4.2');
 
 program
   .command('init [projectName]')
